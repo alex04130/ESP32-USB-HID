408fc6234eb91a76e304d64df01826741b00ecc2
alex04130 <alexya@ydsk.i996icu.com>
2023-11-20 09:03:36 -0600

    脑血栓i2c修复

--- main/main.cpp
+++ main/main.cpp
@@ -43,18 +43,18 @@ static esp_err_t Check_I2C_Error(esp_err_t err)
 {
     switch (err)
     {
-    case ESP_OK: 
+    case ESP_OK:
         break;
-    case ESP_ERR_INVALID_ARG: 
+    case ESP_ERR_INVALID_ARG:
         ESP_LOGE(TAG, "I2C parameter error");
         break;
     case ESP_FAIL:
         ESP_LOGE(TAG, "I2C no slave ACK");
         break;
-    case ESP_ERR_INVALID_STATE: 
+    case ESP_ERR_INVALID_STATE:
         ESP_LOGE(TAG, "I2C driver not installed or not master");
         break;
-    case ESP_ERR_TIMEOUT:  
+    case ESP_ERR_TIMEOUT:
         ESP_LOGE(TAG, "I2C timeout");
         break;
     default:
@@ -68,7 +68,7 @@ esp_err_t bq4050_register_read(uint8_t reg_addr, uint8_t *data, size_t len)
     esp_err_t err = ESP_FAIL;
     i2c_cmd_handle_t cmd = i2c_cmd_link_create();
     i2c_master_start(cmd);
-    i2c_master_write_byte(cmd, BQ4050_ADDR << 1 , ACK_CHECK);
+    i2c_master_write_byte(cmd, BQ4050_ADDR << 1, ACK_CHECK);
     i2c_master_write_byte(cmd, reg_addr, ACK_CHECK);
     i2c_master_start(cmd);
     i2c_master_write_byte(cmd, BQ4050_ADDR << 1 | 1, ACK_CHECK);
@@ -78,7 +78,7 @@ esp_err_t bq4050_register_read(uint8_t reg_addr, uint8_t *data, size_t len)
     }
     i2c_master_read_byte(cmd, &data[len - 1], NACK_VALUE);
     i2c_master_stop(cmd);
-    err = Check_I2C_Error(i2c_master_cmd_begin(I2C_MASTER_NUM, cmd, I2C_MASTER_TIMEOUT_MS/portTICK_PERIOD_MS));
+    err = Check_I2C_Error(i2c_master_cmd_begin(I2C_MASTER_NUM, cmd, I2C_MASTER_TIMEOUT_MS / portTICK_PERIOD_MS));
     i2c_cmd_link_delete(cmd);
     return err;
 }
@@ -252,8 +252,8 @@ static esp_err_t i2c_master_init()
 {
     i2c_config_t conf = {
         .mode = I2C_MODE_MASTER,
-        .sda_io_num = I2C_MASTER_SCL_IO,
-        .scl_io_num = I2C_MASTER_SDA_IO,
+        .sda_io_num = I2C_MASTER_SDA_IO,
+        .scl_io_num = I2C_MASTER_SCL_IO,
         .sda_pullup_en = GPIO_PULLUP_ENABLE,
         .scl_pullup_en = GPIO_PULLUP_ENABLE,
         .master{
